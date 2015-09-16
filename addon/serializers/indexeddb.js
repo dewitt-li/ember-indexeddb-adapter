392f8d17597e71e559e3323adc13983a20a80f9c
wei <wei@Lapis.com>
2015-09-16 14:22:23 -0400

    convert to ember addon

---
+++ addon/serializers/indexeddb.js
@@ -0,0 +1,102 @@
+import Ember from 'ember';
+import DS from 'ember-data';
+ 
+export default  DS.JSONAPISerializer.extend({
+  
+    /**
+    @method serialize
+    @param {DS.Snapshot} snapshot
+    @param {Object} options
+    @return {Object} json
+  */
+  serialize: function(snapshot, options) {
+    var json = {};
+
+    if (options && options.includeId) {
+      var id = snapshot.id;
+
+      if (id) {
+        json[Ember.get(this, 'primaryKey')] = id;
+      }
+    }
+
+    snapshot.eachAttribute(function(key, attribute) {
+      this.serializeAttribute(snapshot, json, key, attribute);
+    }, this);
+
+    snapshot.eachRelationship(function(key, relationship) {
+      if (relationship.kind === 'belongsTo') {
+        this.serializeBelongsTo(snapshot, json, relationship);
+      } else if (relationship.kind === 'hasMany') {
+        this.serializeHasMany(snapshot, json, relationship);
+      }
+    }, this);
+
+    return json;
+  },
+
+
+  /**
+   @method serializeAttribute
+   @param {DS.Snapshot} snapshot
+   @param {Object} json
+   @param {String} key
+   @param {Object} attribute
+  */
+serializeAttribute: function(snapshot, json, key, attribute) {
+    var type = attribute.type;
+
+    if (this._canSerialize(key)) {
+      var value = snapshot.attr(key);
+      if (type) {
+        var transform = this.transformFor(type);
+        value = transform.serialize(value);
+      }
+
+      // if provided, use the mapping provided by `attrs` in
+      // the serializer
+      var payloadKey =  this._getMappedKey(key);
+      json[payloadKey] = value;
+    }
+  },
+  /**
+   @method serializeBelongsTo
+   @param {DS.Snapshot} snapshot
+   @param {Object} json
+   @param {Object} relationship
+  */
+ serializeBelongsTo: function(snapshot, json, relationship) {
+    var key = relationship.key;
+
+    if (this._canSerialize(key)) {
+      var belongsToId = snapshot.belongsTo(key, { id: true });
+
+      // if provided, use the mapping provided by `attrs` in
+      // the serializer
+      var payloadKey = this._getMappedKey(key);
+
+      //Need to check whether the id is there for new&async records
+      if (Ember.isNone(belongsToId)) {
+        json[payloadKey] = null;
+      } else {
+        json[payloadKey] = belongsToId;
+      }
+
+      if (relationship.options.polymorphic) {
+        this.serializePolymorphicType(snapshot, json, relationship);
+      }
+    }
+  },
+
+  /**
+   @method serializeHasMany
+   @param {DS.Snapshot} snapshot
+   @param {Object} json
+   @param {Object} relationship
+  */
+  serializeHasMany: function(snapshot, json, relationship) {
+  },
+  modelNameFromPayloadKey: function(key) {
+    return key;
+  },
+});
